16fb837525bcf29bac4ffa53a18c5434349218d5
江风 <l2495197939@163.com>
2026-03-16 11:32:23 +0800

    Revise gen_boot.sh
    The path contains an extra "targets/", delete it.

--- package/triductor/triboot/src/tools/gen_boot.sh
+++ package/triductor/triboot/src/tools/gen_boot.sh
@@ -10,7 +10,7 @@ dd if=/dev/zero ibs=1k count=200 | tr "\000" "\377" > fileFF.tmp;
 
 cat TR6560-bootimage_nodtb.bin fileFF.tmp  | head --bytes=$BOOT_FAC_LEN > TR6560-bootimage.bin.tmp;
 cat TR6560-bootimage.bin.tmp boot.dtb   > TR6560-bootimage.bin;
-mv TR6560-bootimage.bin $TOPDIR/bin/targets/targets/tr6560/generic/;
+mv TR6560-bootimage.bin $TOPDIR/bin/targets/tr6560/generic/;
 
 rm -f  *.tmp;
 
